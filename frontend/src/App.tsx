ffec996d4f7bd2b2d7bca248f323fbdcfcd34c86
Jesper Svensson <svenssonjesper545@gmail.com>
2026-08-12 13:19:47 +0200

    navbaren och dark/light mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from './components/context/ThemeContext'
 import Sidebar from './components/layout/sidebar'
+import Navbar from './components/layout/navbar'
 import './App.css'
 
-function App(){
-    return(
-        <BrowserRouter>
-        <Sidebar />
-        </BrowserRouter>
+function App() {
+    return (
+        <ThemeProvider>
+            <BrowserRouter>
+                <Navbar />
+                <Sidebar />
+            </BrowserRouter>
+        </ThemeProvider>
     )
 }
-
 
 export default App
